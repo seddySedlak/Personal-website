1b7f4e7c765bc115b144db72d2c2ba4fc8e41295
sedlakt <pan.bramboru@seznam.cz>
2025-06-07 16:04:56 +0200

    adding some animations, smooth scroll, Lenis scroll library

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,19 @@
+// header scroll
 window.addEventListener('scroll', function() {
     const header = document.querySelector('header');
-    if (window.scrollY > 5) {
+    if (window.scrollY > 10) {
         header.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled');
     }
 });
+// smooth html scroll - Lenis library
+const lenis = new Lenis({
+    lerp: 0.185
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
